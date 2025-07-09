a53e28ee07f90b3fc99a02c7b96e0808eca37c1c
lloydestrada <iamlloydestrada@gmail.com>
2025-07-09 14:33:34 +0800

    configure properties

--- src/main/java/com/lloyd/springboot/myApp/rest/FunRestController.java
+++ src/main/java/com/lloyd/springboot/myApp/rest/FunRestController.java
@@ -1,11 +1,26 @@
 package com.lloyd.springboot.myApp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //injecting custom properties name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    //expose new endpoints for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+
+        return  "Coach: " + coachName +  "\nTeam Name: " + teamName;
+    }
 
     //expose "/" return Hello World
     @GetMapping("/")
